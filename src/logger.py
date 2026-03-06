190659cec1e39943191c1f8aab1a24f56bf8caef
zhava77 <zhovnirsky9@gmail.com>
2026-03-06 22:35:55 +0200

    add logging module

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+"""Logging module for event processing."""
+
+import logging
+
+logger = logging.getLogger("event_processing")
+handler = logging.StreamHandler()
+formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
+def log_event(event):
+    """Log an event dispatch."""
+    logger.info("Event dispatched: %s", event)
+
+
+def log_error(message: str):
+    """Log an error message."""
+    logger.error(message)
